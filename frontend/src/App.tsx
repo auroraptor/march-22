bb57258fb89a1deeea922ced12f3e23245cfd0a8
auroraptor <interestingbutno@yandex.ru>
2023-03-19 21:58:39 +0300

    feat: 🐜 added components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,78 @@
 import React from 'react';
+import type { FC } from 'react';
+import { Table, Input } from 'antd';
+import 'antd/dist/reset.css';
+import type { ColumnsType } from 'antd/es/table';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+const {Search} = Input;
+const onSearch = (value: string) => console.log(value);
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+  description: string;
 }
+
+const columns: ColumnsType<DataType> = [
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    render: () => <a>Delete</a>,
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Not Expandable',
+    age: 29,
+    address: 'Jiangsu No. 1 Lake Park',
+    description: 'This not expandable',
+  },
+  {
+    key: 4,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.',
+  },
+];
+
+const App: FC = () => (
+  <div className="App">
+
+  <Search placeholder="input search text" onSearch={onSearch} size="middle" enterButton />
+  <Table
+    columns={columns}
+    expandable={{
+      expandedRowRender: (record) => <p style={{ margin: 0 }}>{record.description}</p>,
+      rowExpandable: (record) => record.name !== 'Not Expandable',
+    }}
+    dataSource={data}
+  />
+    </div>
+
+);
 
 export default App;
